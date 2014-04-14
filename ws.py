d70330ab4f3cfbbaac91e60f89371e4e0daaf756
Undo1 <parker@erwaysoftware.com>
2014-04-13 20:53:10 -0600

    cool language parsing stuff

--- ws.py
+++ ws.py
@@ -2,6 +2,7 @@ import websocket
 import threading
 import json,os,sys,getpass,time
 import HTMLParser
+import nltk
 
 ws = websocket.create_connection("ws://qa.sockets.stackexchange.com/")
 ws.send("155-questions-active")
@@ -10,4 +11,9 @@ while True:
   a=ws.recv()
   d=json.loads(json.loads(a)["data"])
   h = HTMLParser.HTMLParser()
-  print h.unescape(d["titleEncodedFancy"])
+  title = h.unescape(d["titleEncodedFancy"])
+  print title
+
+  tokens = nltk.word_tokenize(title)
+  tagged = nltk.pos_tag(tokens)
+  print tagged[1][1]
