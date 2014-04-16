c54487348f09b4868137aa41951956076f495d3c
Undo1 <parker@erwaysoftware.com>
2014-04-15 19:41:20 -0600

    convert indentation to tabs

--- ws.py
+++ ws.py
@@ -19,33 +19,33 @@ h = HTMLParser.HTMLParser()
 code_to_descriptions = {"CC" : "Coordinating conjunction", "CD" : "Cardinal number", "DT" : "Determiner", "EX" : "Existential there", "FW" : "Foreign word", "IN" : "Preposition or subordinating conjunction", "JJ" : "Adjective", "JJR" : "Comparative adjective", "JJS" : "Superlative adjective", "LS" : "List item marker", "MD" : "Modal", "NN" : "Singular or mass noun", "NNS" : "Plural noun", "NNP" : "Singular proper noun", "NNPS" : "Plural proper noun", "PDT" : "Predeterminer", "POS" : "Possessive ending", "PRP" : "Personal pronoun", "PRP$" : "Possessive pronoun", "RB" : "Adverb", "RBR" : "Comparative adverb", "RBS" : "Superlative adverb", "RP" : "Particle", "SYM" : "Symbol", "TO" : "to", "UH" : "Interjection", "VB" : "Base form Verb", "VBD" : "Past tense verb", "VBG" : "Gerund or present participle verb", "VBN" : "Past participle verb", "VBP" : "Non-3rd person singular present verb", "VBZ" : "3rd person singular present verb", "WDT" : "Wh-determiner", "WP" : "Wh-pronoun", "WP$" : "Possessive wh-pronoun", "WRB" : "Wh-adverb", "." : "Punctuation", "," : "Punctuation"}
 
 while True:
-  a=ws.recv()
-  d=json.loads(json.loads(a)["data"])
-  title = h.unescape(d["titleEncodedFancy"])
+	a=ws.recv()
+	d=json.loads(json.loads(a)["data"])
+	title = h.unescape(d["titleEncodedFancy"])
 
-  cmd = "INSERT INTO titles(title) VALUES(%s)"
+	cmd = "INSERT INTO titles(title) VALUES(%s)"
 
-  cursor.execute(cmd, (title, ))
-  cnx.commit()
+	cursor.execute(cmd, (title, ))
+	cnx.commit()
 
-  print(title)
+	print(title)
 
-  tokens = nltk.word_tokenize(title)
-  tagged = nltk.pos_tag(tokens)
+	tokens = nltk.word_tokenize(title)
+	tagged = nltk.pos_tag(tokens)
 
-  tags = []
+	tags = []
 
-  for word in tagged:
+	for word in tagged:
 	tags.append(word[1])
 
-  groupedtags = [list(g) for k, g in itertools.groupby(sorted(tags))]
+	groupedtags = [list(g) for k, g in itertools.groupby(sorted(tags))]
 
-  for taggroup in groupedtags:
+	for taggroup in groupedtags:
 
-  	description = ""
-  	if taggroup[0] in code_to_descriptions.keys():
-  		description = code_to_descriptions[taggroup[0]]
-  	else:
-  		description = "unknown";
+		description = ""
+		if taggroup[0] in code_to_descriptions.keys():
+			description = code_to_descriptions[taggroup[0]]
+		else:
+			description = "unknown";
 
-  	print(str(len(taggroup)) + " x " + taggroup[0] + " (" + description + ")")
+		print str(len(taggroup)) + " x " + taggroup[0] + " (" + description + ")"
